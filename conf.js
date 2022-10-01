6ff7a7e52b15bbc2bfa2e6dd962c5e7ede72d794
kkskmannn <103875290+kkskmannn@users.noreply.github.com>
2022-10-01 13:09:57 +0000

    Update conf.js

--- conf.js
+++ conf.js
@@ -29,7 +29,7 @@ jsproxy_config({
     'mysite': {
       label: '当前站点',
       lines: {
-        [location.host]: 1,
+        'eee.kkskman125.workers.dev': 1,
       }
     },
     // 该节点用于加载大体积的静态资源
@@ -37,7 +37,7 @@ jsproxy_config({
       label: '',
       hidden: true,
       lines: {
-        'kkskman114.tk': 1,
+        //'kkskman114.tk': 1,
       }
     }
   },
